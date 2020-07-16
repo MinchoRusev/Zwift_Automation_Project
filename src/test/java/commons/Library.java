0818c5bd00b7eaafd174bd933a2a9660aa405e3d
MinchoRusev <68392488+MinchoRusev@users.noreply.github.com>
2020-07-16 16:34:38 -0500

    driver is deleted and WebDriver manager is implemented
    driver is deleted and WebDriver manager is implemented

--- src/test/java/commons/Library.java
+++ src/test/java/commons/Library.java
@@ -1,9 +1,11 @@
 package commons;
 import base.Base;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
@@ -11,16 +13,16 @@ import java.util.concurrent.TimeUnit;
 
 public class Library extends Base {
 
-    String CHROME_DRIVER = "webdriver.chrome.driver";
-    String CHROME_DRIVER_PATH = "C:\\Users\\minch\\IdeaProjects\\ZwiftProject\\src\\test\\java\\drivers\\chromedriver.exe";
-
     public Library(WebDriver _driver) {
         driver = _driver;
     }
 
     public WebDriver openBrowser() {
-        System.setProperty(CHROME_DRIVER, CHROME_DRIVER_PATH);
-        driver = new ChromeDriver();
+
+        WebDriver driver = null;
+        WebDriverManager.chromedriver().version("84.0.4147.89").setup();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
